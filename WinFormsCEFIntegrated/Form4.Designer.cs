d422130967e37bc81124b9d3ddf7d6bf3b9c6a1a
Jashobanta.Pradhan <jashobanta195@gmail.com>
2025-04-25 12:04:58 +0530

    Fore colour

--- WinFormsCEFIntegrated/Form4.Designer.cs
+++ WinFormsCEFIntegrated/Form4.Designer.cs
@@ -95,6 +95,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
             this.label1.Location = new System.Drawing.Point(22, 54);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(62, 14);
@@ -111,6 +112,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
             this.label2.Location = new System.Drawing.Point(22, 92);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(61, 14);
@@ -127,6 +129,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.HotTrack;
             this.label3.Location = new System.Drawing.Point(22, 209);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(46, 14);
